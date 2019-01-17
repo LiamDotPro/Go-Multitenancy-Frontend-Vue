4162ff415293af66b06ddfd7c7716ca0cf49bb76
Liam Read <liam@liam.pro>
2019-01-17 13:52:42 +0000

    Login page updates

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Login from './views/login/Login.vue';
 
 Vue.use(Router);
 
@@ -14,9 +15,9 @@ export default new Router({
             component: Home,
         },
         {
-            path: '/about',
-            name: 'about',
-            component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+            path: '/login',
+            name: 'login',
+            component: Login,
         },
     ],
 });
---
+++ src/views/login/Login.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="Login">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-sm-6 col-lg-4 login-fields-section">
+                    <LoginFields></LoginFields>
+                </div>
+                <div class="col-sm-6 col-lg-8 splash-area d-none d-sm-block ">
+                    <div class="overlay"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import LoginFields from '@/components/login/LoginFields.vue';
+
+    @Component({
+        components: {LoginFields},
+    })
+    export default class Login extends Vue {
+
+    }
+
+</script>
+
+<style type="scss" scoped>
+
+    .login-fields-section {
+        min-height: 100vh;
+        /*background: #f9f9f9;*/
+
+        display: flex;
+        align-items: center;
+        justify-items: center;
+
+        width: 100%;
+        padding: 40px;
+    }
+
+    .overlay {
+        background-color: #588eff;
+        position: relative;
+        width: 100%;
+        height: 100%;
+        filter: contrast(1.75);
+        opacity: 0.5;
+        z-index: 2;
+    }
+
+    .splash-area {
+        background: url("../../assets/icebergs.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: right top;
+        padding: 0;
+    }
+
+</style>
